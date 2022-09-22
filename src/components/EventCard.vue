9bbac8c1cde0c8367b53a5787ec2013be3ebbf9d
koonlookhin <krit_tipnuan@cmu.ac.th>
2022-09-22 11:58:38 +0700

    4.2

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -6,6 +6,8 @@
     <div class="event-card">
       <span>@{{ event.time }} on {{ event.date }}</span>
       <h4>{{ event.title }}</h4>
+      <span>by</span>
+      <h5>{{ event.orgainzer.name }}</h5>
     </div>
   </router-link>
 </template>
