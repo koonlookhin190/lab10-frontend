212da2b5f7988ab24683b9eff9c8ccb49169b403
koonlookhin <krit_tipnuan@cmu.ac.th>
2022-09-22 13:02:16 +0700

    5.2

--- src/views/EventForm.vue
+++ src/views/EventForm.vue
@@ -2,40 +2,44 @@
   <div>
     <h1>Create an event</h1>
     <form @submit.prevent="saveEvent">
-      <label>Category</label>
+      <!-- <label>Category</label>
       <input
         v-model="event.category"
         type="text"
         placeholder="Category"
         class="field"
-      />
+      /> -->
+      <BaseInput v-model="event.category" type="text" label="Category" />
       <h3>Name & describe your event</h3>
 
-      <label>Title</label>
+      <!-- <label>Title</label>
       <input
         v-model="event.title"
         type="text"
         placeholder="Title"
         class="field"
-      />
+      /> -->
+      <BaseInput v-model="event.title" type="text" label="Title" />
 
       <label>Description</label>
-      <input
+      <!-- <input
         v-model="event.description"
         type="text"
         placeholder="Description"
         class="field"
-      />
+      /> -->
+      <BaseInput v-model="event.description" type="text" label="Description" />
 
       <h3>Where is your event?</h3>
 
       <label>Location</label>
-      <input
+      <!-- <input
         v-model="event.location"
         type="text"
         placeholder="Location"
         class="field"
-      />
+      /> -->
+      <BaseInput v-model="event.location" type="text" label="Location" />
       <button type="submit">Submit</button>
     </form>
 
@@ -45,8 +49,12 @@
 
 <script>
 import EventService from '@/services/EventService.js'
+import BaseInput from '@/components/BaseInput.vue'
 export default {
   inject: ['GStore'],
+  components: {
+    BaseInput
+  },
   data() {
     return {
       event: {
