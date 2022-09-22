1f24ac1c82f14df1f84265fc0cb0419dfc26d670
koonlookhin <krit_tipnuan@cmu.ac.th>
2022-09-22 14:04:54 +0700

    decolate form

--- src/views/EventForm.vue
+++ src/views/EventForm.vue
@@ -21,7 +21,7 @@
       /> -->
       <BaseInput v-model="event.title" type="text" label="Title" />
 
-      <label>Description</label>
+      <!-- <label>Description</label> -->
       <!-- <input
         v-model="event.description"
         type="text"
@@ -32,7 +32,7 @@
 
       <h3>Where is your event?</h3>
 
-      <label>Location</label>
+      <!-- <label>Location</label> -->
       <!-- <input
         v-model="event.location"
         type="text"
@@ -325,5 +325,51 @@ select::ms-expand {
 }
 .button.-icon-center {
   padding: 0 20px;
+}
+h3 {
+  font-weight: bold;
+}
+input {
+  width: 100%;
+  height: 40px;
+  margin-bottom: 20px;
+}
+
+label {
+  font-size: 20px;
+  margin-bottom: 5px;
+  text-align: left;
+  font-weight: bold;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+  width: 420px;
+  padding: 20px;
+  margin: auto;
+  margin-top: 40px;
+  border: 2px solid #d8d8d8;
+  border-radius: 10px;
+  background: #7faf58;
+  -webkit-box-shadow: 0px 2px 15px -12px rgba(0, 0, 0, 0.57);
+  -moz-box-shadow: 0px 2px 15px -12px rgba(0, 0, 0, 0.57);
+  box-shadow: 2px 15px -12px rgba(0, 0, 0, 0.57);
+}
+
+button {
+  display: block;
+  margin: auto;
+  margin-top: 25px;
+  background: #ffffff;
+  border: 2px solid #d8d8d8;
+  border-radius: 5px;
+  width: 40%;
+}
+
+@media only screen and (max-width: 600px) {
+  .comment-form {
+    width: 90%;
+  }
 }
 </style>
